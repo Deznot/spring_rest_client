06235dc75684cedae17b7ec1e57e116a470af079
Denis <denis.selitsky@gmail.com>
2023-03-13 15:03:18 +0300

    feat: add MyConfig restTemplate method

--- src/main/java/com/denisselitsky/spring/rest/App.java
+++ src/main/java/com/denisselitsky/spring/rest/App.java
@@ -1,13 +1,18 @@
 package com.denisselitsky.spring.rest;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import com.denisselitsky.spring.rest.configuration.MyConfig;
+import com.denisselitsky.spring.rest.entity.Employee;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.List;
+
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MyConfig.class);
+        Communication communication = context.getBean("communication",  Communication.class);
+        List<Employee> allEmployees = communication.getAllEmployees();
+        System.out.println(allEmployees);
     }
 }
