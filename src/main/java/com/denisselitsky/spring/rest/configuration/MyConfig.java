d30786cdb43977277733fe11e82e4cb04e02f7eb
Denis <denis.selitsky@gmail.com>
2023-03-13 14:07:16 +0300

    init: add start files

---
+++ src/main/java/com/denisselitsky/spring/rest/configuration/MyConfig.java
@@ -0,0 +1,10 @@
+package com.denisselitsky.spring.rest.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.denisselitsky.spring.rest")
+public class MyConfig {
+
+}
